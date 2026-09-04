e98a046fb7d7e57a1190b1fcd3f3fdc7f32e4a30
Dilmith-Ranasinghe518 <it23249998@my.sliit.lk>
2026-09-04 16:15:23 +0530

    Fix TypeError: null checks for scoreLabel, streakLabel, timerLabel, ratingLabel in games

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH !== undefined
   : (isProd ? '/sampath' : '');
 
 const nextConfig = {
-  distDir: isProd ? '.next_prod' : '.next',
   ...(isProd ? { output: 'export', trailingSlash: true } : {}),
   basePath: basePath || undefined,
   images: {
